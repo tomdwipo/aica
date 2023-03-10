1adbaea9a051bce5a7029f8763a8bbc6d0781447
Bambang Cahyo Soetrisno <bambang.cahyo@amarbank.co.id>
2023-03-10 21:28:25 +0700

    fix(login): Fix simplfy back button

--- PlaygroundApp/PlaygroundApp/Login/LoginViewController.swift
+++ PlaygroundApp/PlaygroundApp/Login/LoginViewController.swift
@@ -12,30 +12,20 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var lblButtonCreateAccount: UILabel!
     
-    @IBOutlet weak var btnBack: UIImageView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationItem.setHidesBackButton(true, animated: true)
+        let backButton = UIBarButtonItem()
+        backButton.tintColor = UIColor.gray
+        self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
         
         let tapLblCreateAccount = UITapGestureRecognizer(target: self, action: #selector(navigateToSignUpPage))
         self.lblButtonCreateAccount.addGestureRecognizer(tapLblCreateAccount)
         self.lblButtonCreateAccount.isUserInteractionEnabled = true
-        
-        
-        let tapBtnBack = UITapGestureRecognizer(target: self, action: #selector(navigateBack))
-        self.btnBack.addGestureRecognizer(tapBtnBack)
-        self.btnBack.isUserInteractionEnabled = true
     }
     
     @objc func navigateToSignUpPage() {
         let vc = SignUpViewController(nibName: "SignUpView", bundle: nil)
         self.navigationController?.pushViewController(vc, animated: true)
     }
-    
-    @objc func navigateBack() {
-        self.navigationController?.popViewController(animated: true)
-    }
-
 }
